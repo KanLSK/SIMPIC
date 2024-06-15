af97c7f928c1bd7255453037d1d821bcf185f775
Kan Halder <59249490+KanLSK@users.noreply.github.com>
2024-06-15 15:18:40 +0700

    more than competition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import pic from '../images/pic.jpg';
 import bird from '../images/bird.jpeg';
 import square from '../images/square.svg';
@@ -8,13 +8,36 @@ import { NavLink } from 'react-router-dom';
 
 const Home = ({ user }) => {
 
+  const [mouseEnter, setMouseEnter] = useState("zero")
+  const [bgColor, setBgColor] = useState("bg-[rgba(217,217,217,1)]")
+
+  useEffect(() => {
+    console.log(mouseEnter == "two")
+
+    if (mouseEnter === "one") {
+      setBgColor("bg-[rgb(239,238,220)]")
+    } else if (mouseEnter === "two") {
+      setBgColor("bg-[rgb(220,233,194)]")
+    } else if (mouseEnter === "three") {
+      setBgColor("bg-[rgb(211,219,252)]")
+    } else if (mouseEnter === "four") {
+      setBgColor("bg-[rgb(251,234,254)]")
+    } else if (mouseEnter === "five") {
+      setBgColor("bg-[rgb(223,241,254)]")
+    } else if (mouseEnter === "six") {
+      setBgColor("bg-[rgb(123,152,117)]")
+    } else {
+      setBgColor("bg-[rgba(217,217,217,1)]")
+    }
+  }, [mouseEnter]);
+
   return (
-    <div className='min-w-screen min-h-screen bg-[rgba(255,255,255,1)] flex flex-col'>
+    <div className='min-w-screen min-h-screen bg-[rgba(255,255,255,1)] flex flex-col items-center font-inter'>
         <div className='relative bg-cover w-screen h-[750px] flex justify-center items'>
             <img src={pic} alt="microbio" className='h-[750px] w-screen opacity-40 bg-fixed'/>
             {/** when login */}
             <div className='absolute text-center flex flex-col gap-3'>
-              <div className='relative w-screen h-[750px] flex flex-col justify-center items-center gap-4'>
+              <div className='relative w-screen max-w-screen-2xl h-[750px] flex flex-col justify-center items-center gap-4'>
                 <p className='text-[64px] text-black font-extrabold'>SIMPIC 2025</p>
                 <p className='text-[24px]'>
                   The 14th Siriraj International Medical Microbiology, Parasitology, and <br/>Immunology Competition
@@ -24,7 +47,7 @@ const Home = ({ user }) => {
             </div>
         </div>
         {/** first section */}
-        <section className='flex flex-wrap w-full justify-between items-center h-[500px] px-10 md:px-20'>
+        <section className='flex flex-wrap w-full max-w-screen-2xl justify-between items-center h-[500px] px-10 md:px-20'>
            <div className='flex flex-col gap-5'>
                 <p className='font-bold text-4xl'>The new style competition</p>
                 <p className='font-light text-gray-500'>The new style of competition never seen before!</p>
@@ -35,7 +58,7 @@ const Home = ({ user }) => {
            </div>
         </section>
         {/** second section */}
-        <section className='flex flex-wrap w-full h-fit justify-between items-center px-10 md:px-20'>
+        <section className='flex flex-wrap w-full max-w-screen-2xl h-fit justify-between items-center px-10 md:px-20'>
           <div>
             <img src={square} alt="scare" />
           </div>
@@ -67,8 +90,70 @@ const Home = ({ user }) => {
           </div>
         </section>
         {/** third section */}
-        <section className='flex flex-col w-full h-fit justify-between items-center bg-[rgba(217,217,217,1)] mt-[6em]'>
-          
+        <section className='flex flex-col w-screen max-w-screen-2xl justify-between items-center mt-[7em] px-20'>
+          <div className={`${bgColor} h-[675px] w-full flex flex-col justify-between rounded-lg`}>
+            <p className='text-4xl font-bold p-8'>More than a competition</p>
+            <div>
+              <div className='flex w-full justify-around'>
+                <div className='transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 bg-[rgba(255,171,171,1)] hover:bg-[rgb(230,193,205)]  px-3 py-4 rounded-lg' onMouseEnter={() => setMouseEnter("one")} onMouseLeave={() => setMouseEnter("zero")}>
+                  <p className='text-lg mb-2'>Welcome Dinner</p>
+                  <p className='text-[rgba(130,130,130,1)] font-extralight'>
+                    Body text for whatever you'd like to type. Add main
+                    <br/>takeaway points, quotes, anecdotes, or even a very
+                    <br/>very short story.
+                  </p>
+                </div>
+                <div className='transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 bg-[rgba(255,171,171,1)] hover:bg-[rgb(230,193,205)]  px-3 py-4 rounded-lg' onMouseEnter={() => setMouseEnter("two")} onMouseLeave={() => setMouseEnter("zero")}>
+                  <p className='text-lg mb-2'>Conference</p>
+                  <p className='text-[rgba(130,130,130,1)] font-extralight'>
+                    Body text for whatever you'd like to type. Add main
+                    <br/>takeaway points, quotes, anecdotes, of even a very
+                    <br/>very short story.
+                  </p>
+                </div>
+                <div className='transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 bg-[rgba(255,171,171,1)] hover:bg-[rgb(230,193,205)]  px-3 py-4 rounded-lg' onMouseEnter={() => setMouseEnter("three")} onMouseLeave={() => setMouseEnter("zero")}>
+                  <p className='text-lg mb-2'>Night Party</p>
+                  <p className='text-[rgba(130,130,130,1)] font-extralight'>
+                    Body text for whatever you'd like to type. Add main
+                    <br/>takeaway points, quotes, anecdotes, of even a very
+                    <br/>very short story.
+                  </p>
+                </div>
+              </div>
+              <div className='flex w-full justify-around mt-10 mb-10'>
+                <div className='transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 bg-[rgba(255,171,171,1)] hover:bg-[rgb(230,193,205)] px-3 py-4 rounded-lg' onMouseEnter={() => setMouseEnter("four")} onMouseLeave={() => setMouseEnter("zero")}>
+                  <p className='text-lg mb-2'>Museum Tour</p>
+                  <p className='text-[rgba(130,130,130,1)] font-extralight'>
+                    Body text for whatever you'd like to type. Add main
+                    <br/>takeaway points, quotes, anecdotes, of even a very
+                    <br/>very short story.
+                  </p>
+                </div>
+                <div className='transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 bg-[rgba(255,171,171,1)] hover:bg-[rgb(230,193,205)]  px-3 py-4 rounded-lg' onMouseEnter={() => setMouseEnter("five")} onMouseLeave={() => setMouseEnter("zero")}>
+                  <p className='text-lg mb-2'>Cultural Excursion</p>
+                  <p className='text-[rgba(130,130,130,1)] font-extralight'>
+                    Body text for whatever you'd like to type. Add main
+                    <br/>takeaway points, quotes, anecdotes, of even a very
+                    <br/>very short story.
+                  </p>
+                </div>
+                <div className='transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 bg-[rgba(255,171,171,1)] hover:bg-[rgb(230,193,205)]  px-3 py-4 rounded-lg' onMouseEnter={() => setMouseEnter("six")} onMouseLeave={() => setMouseEnter("zero")}> 
+                  <p className='text-lg mb-2'>Community</p>
+                  <p className='text-[rgba(130,130,130,1)] font-extralight'>
+                    Body text for whatever you'd like to type. Add main
+                    <br/>takeaway points, quotes, anecdotes, of even a very
+                    <br/>very short story.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/** fourth section */}
+        <section className='flex flex-col w-screen max-w-screen-2xl justify-between items-center mt-[7em] px-20'>
+          <div>
+            <p className='text-4xl font-bold'>What happens at SIMPIC last year</p>
+          </div>
         </section>
     </div>
   )
